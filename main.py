de60274c40e9e29e2fe10fde262525b8ddcfc013
1millionmoon <1millionmoon@gmail.com>
2024-10-16 13:14:26 +0700

    add error handling, stop force close program

--- main.py
+++ main.py
@@ -8,16 +8,21 @@ from pprint import pprint
 from uuid import UUID
 import configparser
 
-config = configparser.ConfigParser()
-config.read('config.txt')
-debug = config['twitch']['debug']
-APP_ID = config['twitch']['client_id']
-APP_SECRET = config['twitch']['client_secret']
+try  :
+    config = configparser.ConfigParser()
+    config.read('config.txt')
+    debug = config['twitch']['debug']
+    APP_ID = config['twitch']['client_id']
+    APP_SECRET = config['twitch']['client_secret']
+    TARGET_CHANNEL = config['twitch']['channel']
+    USER_SCOPE = [AuthScope.CHANNEL_READ_REDEMPTIONS]
+    if (debug=='True'):
+        USER_SCOPE = [AuthScope.CHANNEL_MODERATE, AuthScope.CHANNEL_READ_REDEMPTIONS]
 
-TARGET_CHANNEL = config['twitch']['channel']
-USER_SCOPE = [AuthScope.CHANNEL_READ_REDEMPTIONS]
-if (debug=='True'):
-    USER_SCOPE = [AuthScope.CHANNEL_MODERATE, AuthScope.CHANNEL_READ_REDEMPTIONS]
+except BaseException as e: 
+    print("config.txt error")
+    print(str(e))
+
 
 async def callback_whisper(uuid: UUID, data: dict) -> None:
     print('got callback for UUID ' + str(uuid))
@@ -29,6 +34,7 @@ async def run_example():
     twitch = await Twitch(APP_ID, APP_SECRET)
     auth = UserAuthenticator(twitch, USER_SCOPE, force_verify=False)
     token, refresh_token = await auth.authenticate()
+
     # you can get your user auth token and user auth refresh token following the example in twitchAPI.oauth
     await twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
     user = await first(twitch.get_users(logins=[TARGET_CHANNEL]))
@@ -36,25 +42,38 @@ async def run_example():
     # starting up PubSub
     pubsub = PubSub(twitch)
     pubsub.start()
+
     if (debug=='False'):
         try:
             reward_list = await twitch.get_custom_reward(user.id, reward_id=None, only_manageable_rewards=False)
             pprint(reward_list)
-        except:
+        except BaseException as e:
             print("Get redeem list error")
+            print(str(e))
 
     # you can either start listening before or after you started pubsub.
-    if (debug=='True'):
-        uuid = await pubsub.listen_chat_moderator_actions(user.id, user.id ,callback_whisper)
-    else:
-        uuid = await pubsub.listen_channel_points(user.id ,callback_whisper)
-    input('program is running . press ENTER to close...\n')
-    print('closing program...')
+    try : 
+        if (debug=='False'):
+            uuid = await pubsub.listen_channel_points(user.id ,callback_whisper)
+        else:
+            uuid = await pubsub.listen_chat_moderator_actions(user.id, user.id ,callback_whisper)
+    except BaseException as e: 
+        print("PubSub listen error")
+        print(str(e))
+
+    input('program is running . press ENTER to shut down program...')
+    print('\nClosing program...')
     # you do not need to unlisten to topics before stopping but you can listen and unlisten at any moment you want
     await pubsub.unlisten(uuid)
     pubsub.stop()
     await twitch.close()
+
+try : 
+    asyncio.run(run_example())
+except BaseException as e : 
+    print("General Error")
+    print(str(e))
+finally : 
     input('press ENTER to close...\n')
 
-asyncio.run(run_example())
 
